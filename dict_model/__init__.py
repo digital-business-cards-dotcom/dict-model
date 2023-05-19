dc4ba33cce80532aa6b84d74bbbe692726108f4e
bolo-zero <bolozero@proton.me>
2023-05-18 17:24:18 -0700

    update release version

--- dict_model/__init__.py
+++ dict_model/__init__.py
@@ -10,7 +10,7 @@ from django.utils.functional import classproperty
 from . import deserializers, lookup, serializers
 from .query_sets import DictModelQuerySet
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 @dataclasses.dataclass(kw_only=True)
